47254dd8535ff33b62879a5db876e454f3a101b4
Nobu Funaki <nobu@sidefeed.com>
2017-01-13 11:50:20 -0800

    add User()

---
+++ examples/users/users.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/nobuf/cas"
+	"log"
+	"os"
+)
+
+func main() {
+	api := cas.New(os.Getenv("TWITCASTING_API_CLIENT_ID"), os.Getenv("TWITCASTING_API_CLIENT_SECRET"))
+	u, err := api.User(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%v\n", u)
+}
