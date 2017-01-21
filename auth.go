5843fd6b4680c8f520365acfbe6e8453cfc40057
Nobu Funaki <nobuf@users.noreply.github.com>
2017-01-20 17:46:35 -0800

    add api.hasAuthToken() (#7)

--- auth.go
+++ auth.go
@@ -9,6 +9,14 @@ func (api *Client) basicAuthToken() string {
 	return base64.StdEncoding.EncodeToString([]byte(data))
 }
 
+func (api *Client) hasAuthToken() bool {
+	if len(api.clientID) > 0 && len(api.clientSecret) > 0 {
+		return true
+	}
+	return false
+}
+
+// Return Authorization http header
 func (api *Client) auth() string {
 	// TODO use access_token if it's available
 	return "Basic " + api.basicAuthToken()
