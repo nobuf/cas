2ccdde2a4c6be6a999b4b7618a33ce82a152eda6
Nobu Funaki <nobuf@users.noreply.github.com>
2017-01-20 21:47:24 -0800

    Better documenting (#8)
    * add more comments following golint
    
    * update README

--- search.go
+++ search.go
@@ -1,7 +1,10 @@
 package cas
 
+// SearchRecommendLives retrieves recommended Moves on live
+// See http://apiv2-doc.twitcasting.tv/#search-live-movies
 func (api *Client) SearchRecommendLives() (Movies, error) {
 	movies := Movies{}
+	// TODO support parameters
 	err := get(api, "/search/lives?type=recommend&lang=ja&limit=50", &movies)
 	if err != nil {
 		return Movies{}, err
